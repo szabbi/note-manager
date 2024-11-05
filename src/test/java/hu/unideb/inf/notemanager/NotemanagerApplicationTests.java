2a900af10cf0413da0780796865304c6b5484b99
Nagy Szabolcs <nagy.szabolcsffs@gmail.com>
2024-11-05 15:21:03 +0100

    initial

---
+++ src/test/java/hu/unideb/inf/notemanager/NotemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.notemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
